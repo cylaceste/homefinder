d9c2df4ff72054bc1c3a29345b838be99f2f9f7d
weijie2 <jeremykid1992@gmail.com>
2023-07-22 14:59:22 -0600

    Add insert and get sql command

--- homefinder/backend/database_prepare.py
+++ homefinder/backend/database_prepare.py
@@ -1,4 +1,6 @@
 import sqlite3 as sq
+import pandas as pd
+
 
 class sql_db():
     def __init__(self):
@@ -13,11 +15,11 @@ class sql_db():
                 property_name VARCHAR(255),
                 description VARCHAR(255),
                 num_bedroom INT,
-                num_bathroom INT,
+                num_bathroom Float,
                 area_size INT,
                 price FLOAT(10,2),
-                transaction_type TEXT CHECK( property_type IN ('Buy', 'Rent') ),
-                property_types TEXT CHECK( property_types IN ('Condo', 'Apartment', 'House') ),
+                transaction_type TEXT CHECK( transaction_type IN ('Buy', 'Rent') ),
+                property_types TEXT CHECK( property_types IN ('Condo', 'Apartment', 'House', 'Townhouse') ),
                 parking TEXT CHECK( parking IN ('garage', 'underground', 'covered', 'outdoor') ),
                 laundry TEXT CHECK( laundry IN ('in_suite', 'shared') ),
                 furnished BOOL,
@@ -25,7 +27,6 @@ class sql_db():
                 longtidude decimal(10,7),
                 latitude decimal(10,7),
                 build_year YEAR,
-                smoking_allowed Bool,
                 air_conditioning Bool,
                 hardwood_floors Bool,
                 balcony Bool
@@ -33,12 +34,55 @@ class sql_db():
         ''')
         self.conn.commit()
 
+    def convert_csv_to_sql(self, table_name='property_table', csv_file_name='property_table.csv'):
+        sql_script = ""
+        if table_name == 'property_table':
+            df = pd.read_csv(csv_file_name)
+            print (df.columns.tolist())
+            for row in df.itertuples():
+                sql_script = '''
+                    INSERT INTO property_table (property_id, property_name, description, num_bedroom, num_bathroom, 
+                    area_size, price, transaction_type, property_types, parking, laundry, furnished, pet_friendly,
+                    longtidude, latitude, build_year, air_conditioning, hardwood_floors, balcony)
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                    '''
+                    #
+                record = (row.property_id, 
+                    row.property_name,
+                    row.description,
+                    row.num_bedroom,
+                    row.num_bathroom,
+                    row.area_size,
+                    row.price,
+                    row.transaction_type,
+                    row.property_types,
+                    row.parking,
+                    row.laundry,
+                    row.furnished,                    
+                    row.pet_friendly,
+                    row.longitude,
+                    row.latitude,
+                    row.build_year,
+                    row.air_conditioning,
+                    row.hardwood_floors,
+                    row.balcony)
+                self.c.execute(sql_script, record)
+            self.conn.commit()
+        else:
+            pass
+        return 
+
+    def get_list_properties(self, sql_command="SELECT * FROM property_table;"):
+        self.c .execute(sql_command) 
+        data = self.c.fetchall()
+        return data
+
     def close(self):
         self.conn.close()
 
-
-
-
 if __name__ == "__main__":
     sql_class = sql_db()
+    sql_class.convert_csv_to_sql( table_name='property_table', csv_file_name='../database/property_table.csv')
+    data = sql_class.get_list_properties()
+    print (data)
     sql_class.close()
