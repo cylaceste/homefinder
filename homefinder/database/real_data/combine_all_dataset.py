589dda045718d7acd5785f1df9120334ebe0b3c1
weijie2 <jeremykid1992@gmail.com>
2023-07-23 16:22:27 -0600

    finish test

--- homefinder/database/real_data/combine_all_dataset.py
+++ homefinder/database/real_data/combine_all_dataset.py
@@ -10,10 +10,10 @@ for file in file_list:
 
     property_table_files.append(pd.read_csv(file+'_property.csv'))
     image_table_files.append(pd.read_csv(file+'_image.csv'))
-    agent_table_files.append(pd.read_csv(file+'_agent.csv'))
+    # agent_table_files.append(pd.read_csv(file+'_agent.csv'))
     
 print ()
 property_df = pd.concat(property_table_files)
 property_df.to_csv(target_path+'property_table.csv')
 pd.concat(image_table_files).to_csv(target_path+'image_table.csv')
-pd.concat(agent_table_files).to_csv(target_path+'agent_table.csv')
+# pd.concat(agent_table_files).to_csv(target_path+'agent_table.csv')
